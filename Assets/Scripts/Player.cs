7b89afee9090e871bac04f4cf61f62b9ad80f98d
Nikhil Sharma <nik41507@gmail.com>
2018-10-16 14:51:51 -0700

    Jumping Test
    -Added the ability to a player to jump by pressing "A"
    -This is just a test so they can fly by spamming A and move while jumping and all tha stuff
    -Allowed WASD movement for Player 1 for any of us to test on our computers if we are ever without a controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,40 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     //This will control everything relating to the player 
 
-   [SerializeField ]
-   float speed;
-   public bool lerp = false;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpForce;
 
-   Vector3 forward, right;
 
-   [Tooltip("Represents which player this is. Only put in 1-4. Do not put 0!!! This attribute must have a value in order to work or take in input properly!!! ")]
-   public int playerNum;
-   
-   // These are the suffixes used to form a string that represents a specific input on a specific Xbox controller.
-   // The suffixes represent the types of inputs found on an Xbox controller (NOTE: more might be added later in the script).
-   [HideInInspector]
-   public string left_Joystick_X_Axis = "LeftJoystickX";
-   [HideInInspector]
-   public string left_Joystick_Y_Axis = "LeftJoystickY";
-   [HideInInspector]
-   public string Dpad_X_Axis = "DpadX";
-   [HideInInspector]
-   public string Dpad_Y_Axis = "DpadY";
+    Vector3 forward, right;
 
-   // These are the prefixes used to form a string that represents a specific input on a specific Xbox controller.
-   // The prefixes represent which Xbox controller an instance of this script should take input from.
-   [HideInInspector]
-   public const string Player_1_Str = "P1_";
-   [HideInInspector]
-   public const string Player_2_Str = "P2_";
-   [HideInInspector]
-   public const string Player_3_Str = "P3_";
-   [HideInInspector]
-   public const string Player_4_Str = "P4_";
+    [Tooltip("Represents which player this is. Only put in 1-4. Do not put 0!!! This attribute must have a value in order to work or take in input properly!!! ")]
+    public int playerNum;
 
-   private string playerPrefix;
-    
-	void Start () {
+    // These are the suffixes used to form a string that represents a specific input on a specific Xbox controller.
+    // The suffixes represent the types of inputs found on an Xbox controller (NOTE: more might be added later in the script).
+    [HideInInspector]
+    public string left_Joystick_X_Axis = "LeftJoystickX";
+    [HideInInspector]
+    public string left_Joystick_Y_Axis = "LeftJoystickY";
+    [HideInInspector]
+    public string Dpad_X_Axis = "DpadX";
+    [HideInInspector]
+    public string Dpad_Y_Axis = "DpadY";
+
+    // These are the prefixes used to form a string that represents a specific input on a specific Xbox controller.
+    // The prefixes represent which Xbox controller an instance of this script should take input from.
+    [HideInInspector]
+    public const string Player_1_Str = "P1_";
+    [HideInInspector]
+    public const string Player_2_Str = "P2_";
+    [HideInInspector]
+    public const string Player_3_Str = "P3_";
+    [HideInInspector]
+    public const string Player_4_Str = "P4_";
+
+    private string playerPrefix;
+
+    public Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        //Physics.gravity = new Vector3(0, -15, 0);
         playerPrefix = "";
         // Decides which player to take input from if the correct input is given.
         switch (playerNum)
@@ -63,15 +69,15 @@ public class Player : MonoBehaviour {
                 break;
         }
 
-        forward = Camera.main.transform.forward; 
+        forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // This right vector is -45 degrees from the world X axis 
-	}
+    }
 
     void Move(string horizontal, string vertical)
     {
-        Debug.Log(horizontal);
+        //Debug.Log(horizontal);
         Vector3 direction = new Vector3(Input.GetAxis(horizontal), 0, Input.GetAxis(vertical));
         Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis(horizontal);
         Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis(vertical);
@@ -80,19 +86,45 @@ public class Player : MonoBehaviour {
         transform.position += rightMovement;
         transform.position += upMovement;
     }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void Attack()
+    {
+
+    }
 	
-	// Update is called once per frame
+    // Update is called once per frame
 	void Update () {
+
+        //Checking for Movement
         // only for testing a single player with keyboard if you dont have an Xbox controller!
         // Otherwise, comment out the first if-else block.
-        if (Input.GetAxis("Horizontal") != 0.0 | Input.GetAxis("Vertical") != 0.0) //Left Joystick and WASD
-            Move("Horizontal","Vertical");
-        else if (Input.GetAxis("HorizontalKey") != 0.0 | Input.GetAxis("VerticalKey") != 0.0) //D-Pad
-            Move("HorizontalKey", "VerticalKey");
-
+        if (Input.GetAxis("Horizontal") != 0.0 | Input.GetAxis("Vertical") != 0.0) //WASD only for the first player
+            if(playerNum == 1)
+                Move("Horizontal","Vertical");
         if (Input.GetAxis(playerPrefix + left_Joystick_X_Axis) != 0.0 | Input.GetAxis(playerPrefix + left_Joystick_Y_Axis) != 0.0) //Left Joystick
             Move(playerPrefix + left_Joystick_X_Axis, playerPrefix + left_Joystick_Y_Axis);
         else if (Input.GetAxis(playerPrefix + Dpad_X_Axis) != 0.0 | Input.GetAxis(playerPrefix + Dpad_Y_Axis) != 0.0) //D-Pad
             Move(playerPrefix + Dpad_X_Axis, playerPrefix + Dpad_Y_Axis);
-	}
+        
+        
+        //Checking for jumping
+        if (Input.GetButtonDown(playerPrefix + "A"))
+        {
+            Jump();
+        }
+
+        //Checking for attacking
+        if (Input.GetButtonDown(playerPrefix + "RightBumper"))
+        {
+            Debug.Log("Trying to attack!");
+        }
+
+
+
+    }
 }
