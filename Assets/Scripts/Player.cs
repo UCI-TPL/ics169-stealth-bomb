513d5518ebb136035de006b5d3081639f60391ba
Brian Lam <BrianLam4101@gmail.com>
2018-10-18 23:25:21 -0700

    Split Player.cs into 3 scripts
    Split Player.cs into Player.cs, PlayerController.cs, and PlayerStats.cs
    - Player.cs is for managing the overall player, such as spawning, dying.  It also acts as a general interface for other scripts, such as recieving damage
    - PlayerController.cs handles player controls (moving, jumping, aiming, attacking, and dodging)
    - PlayerStats.cs holds all the player's stats and handles modifying them, such as through power-ups/debuffs and being hurt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,192 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Player requires the GameObject to have a PlayerController and PlayerStats component
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(PlayerStats))]
 public class Player : MonoBehaviour {
-    //This will control everything relating to the player 
 
-    [SerializeField]
-    float speed;
-    [SerializeField]
-    float jumpForce;
-    [SerializeField]
-    int health = 100;
-    [SerializeField]
-    float shootRate = 0.2f;
-
-    float shootTime = 0.0f;
-
-    bool isGrounded;
-
-    public GameObject ShootPoint;
-    public Rigidbody rb;
-    public Projectile arrow; //this is used for the Basic Attack
-
-    Vector3 forward, right;
-
-    [Tooltip("Represents which player this is. Only put in 1-4. Do not put 0!!! This attribute must have a value in order to work or take in input properly!!! ")]
-    public int playerNum;
-
-    // These are the suffixes used to form a string that represents a specific input on a specific Xbox controller.
-    // The suffixes represent the types of inputs found on an Xbox controller (NOTE: more might be added later in the script).
-    [HideInInspector]
-    public string left_Joystick_X_Axis = "LeftJoystickX";
-    [HideInInspector]
-    public string left_Joystick_Y_Axis = "LeftJoystickY";
-    [HideInInspector]
-    public string right_Joystick_X_Axis = "RightJoystickX";
-    [HideInInspector]
-    public string right_Joystick_Y_Axis = "RightJoystickY";
-    [HideInInspector]
-    public string leftBumper = "LeftBumper";
-    [HideInInspector]
-    public string rightBumper = "RightBumper";
-    [HideInInspector]
-    public string leftTrigger = "LeftTrigger";
-    [HideInInspector]
-    public string rightTrigger = "RightTrigger";
-    [HideInInspector]
-    public string Dpad_X_Axis = "DpadX";
-    [HideInInspector]
-    public string Dpad_Y_Axis = "DpadY";
-
-    // These are the prefixes used to form a string that represents a specific input on a specific Xbox controller.
-    // The prefixes represent which Xbox controller an instance of this script should take input from.
-    [HideInInspector]
-    public const string Player_1_Str = "P1_";
-    [HideInInspector]
-    public const string Player_2_Str = "P2_";
-    [HideInInspector]
-    public const string Player_3_Str = "P3_";
-    [HideInInspector]
-    public const string Player_4_Str = "P4_";
-
-    private string playerPrefix;
-
-    
-
-    void Start() {
-        rb = GetComponent<Rigidbody>();
-        //Physics.gravity = new Vector3(0, -15, 0);
-        playerPrefix = "";
-        // Decides which player to take input from if the correct input is given.
-        switch (playerNum)
-        {
-            case 0:
-                Debug.Log("Either the player number (playerNum) was not assigned or you put in 0. Both are in invalid");
-                break;
-            case 1:
-                playerPrefix = Player_1_Str;
-                break;
-            case 2:
-                playerPrefix = Player_2_Str;
-                break;
-            case 3:
-                playerPrefix = Player_3_Str;
-                break;
-            case 4:
-                playerPrefix = Player_4_Str;
-                break;
-            default:
-                Debug.Log("This game does not support more than 4 players.");
-                break;
-        }
-
-        forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // This right vector is -45 degrees from the world X axis 
+    private PlayerStats _playerStats;
+    public PlayerStats playerStats {
+        get { return _playerStats; }
+    }
+    private PlayerController _playerController;
+    public PlayerController playerController {
+        get { return _playerController; }
     }
 
-    void Move(string horizontal, string vertical)
-    {
-        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis(horizontal);
-        Vector3 upMovement = forward * speed * Time.deltaTime * Input.GetAxis(vertical);
-        transform.position += rightMovement;
-        transform.position += upMovement;
+    private void Awake() {
+        _playerStats = GetComponent<PlayerStats>();
+        if (playerStats == null) // Check to ensure PlayerStats component is present, since PlayerStats is a dependency this will never happen, but just in case
+            Debug.LogError(gameObject.name + " missing PlayerStats Component");
+        _playerController = GetComponent<PlayerController>();
+        if (playerController == null) // Check to ensure PlayerController component is present, since PlayerController is a dependency this will never happen, but just in case
+            Debug.LogError(gameObject.name + " missing PlayerController Component");
     }
 
-    void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isGrounded = false;
+    public void HurtPlayer(int damage) {
+        playerStats.health -= damage;
     }
 
-    void Attack()
-    {
-        StartCoroutine("Shoot");   
-    }
-
-    IEnumerator Shoot()
-    {
-        if(shootTime <= Time.time)
-        {
-            Instantiate(arrow, ShootPoint.transform.position, transform.rotation, null); //this instantiates the arrow as an attack
-            shootTime = Time.time + shootRate;
-            yield return new WaitForSeconds(shootRate);
-        }
-        //yield return new WaitForSeconds(shootRate);
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        isGrounded = true;
-    }
-
-    void RotatePlayer(string horizontal, string vertical)
-    {
-        Vector3 rightMovement = right * Time.deltaTime * Input.GetAxis(horizontal);
-        Vector3 upMovement = forward * Time.deltaTime * Input.GetAxis(vertical);
-        transform.forward = Vector3.Normalize(rightMovement - upMovement);
-    }
-
-
-    public void HurtPlayer(int damage)
-    {
-        health -= damage;
-    }
-
-    void CheckDeath()
-    {
-        if (health <= 0)
-        {
+    private void CheckDeath() {
+        if (playerStats.health <= 0) {
             //Debug.Log("D E A T H ");
             Destroy(gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update () {
-
-        //Checking for Movement
-        // only for testing a single player with keyboard if you dont have an Xbox controller!
-        // Otherwise, comment out the first if-else block.
-        if (Input.GetAxis("Horizontal") != 0.0 | Input.GetAxis("Vertical") != 0.0) //WASD only for the first player
-            if(playerNum == 1)
-                Move("Horizontal","Vertical");
-        if (Input.GetAxis(playerPrefix + left_Joystick_X_Axis) != 0.0 | Input.GetAxis(playerPrefix + left_Joystick_Y_Axis) != 0.0) //Left Joystick
-            Move(playerPrefix + left_Joystick_X_Axis, playerPrefix + left_Joystick_Y_Axis);
-        else if (Input.GetAxis(playerPrefix + Dpad_X_Axis) != 0.0 | Input.GetAxis(playerPrefix + Dpad_Y_Axis) != 0.0) //D-Pad
-            Move(playerPrefix + Dpad_X_Axis, playerPrefix + Dpad_Y_Axis);
-        
-        if(Input.GetAxis(playerPrefix + right_Joystick_X_Axis) != 0.0 | Input.GetAxis(playerPrefix + right_Joystick_Y_Axis) != 0.0) //rotation of player with right stick
-        {
-            RotatePlayer(playerPrefix + right_Joystick_X_Axis, playerPrefix + right_Joystick_Y_Axis);
-        }
-        
-        //Checking for jumping
-        if (Input.GetButtonDown(playerPrefix + rightBumper) && isGrounded)
-        {
-            Jump();
-        }
-
-        //Checking for attacking
-        if (Input.GetAxis(playerPrefix + rightTrigger) != 0.0)
-        {
-            Attack();
-        }
-
-
+    void Update() {
         CheckDeath();
     }
 }
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    private Player _player;
+    public Player player {
+        get { return _player; }
+    }
+
+    private void Awake() {
+        _player = GetComponent<Player>();
+        if (player == null) // Check to ensure Player component is present, since PlayerStats is a dependency of Player this will never happen, but just in case
+            Debug.LogError(gameObject.name + " missing Player Component");
+    }
+    
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    public float jumpForce;
+    [SerializeField]
+    public int health = 100;
+}
