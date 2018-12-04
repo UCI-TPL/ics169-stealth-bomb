dc35e522092bb01e8643b5b773186f6a1b117636
d-guzman <dghost121@gmail.com>
2018-12-04 02:54:08 -0800

    Implemented Bomb Weapon
    - Created a Bomb Weapon (Grenade Launcher) and the associated scripts
    (Bomb, BombWeapon, BombWeaponData)
    - Created a Grenade Launcher with the following properties:
    - Limited use (only 5 as of now; value can change) AOE weapon with high
    knockback.
    - Blast only hits players once.
    - When exhausted, it will be replaced with the weapon the player picked
    up.
    
    --- POSSIBLE ISSUE?
    - If a player holds onto an limited use weapon into the next round, they
    will not have a weapon when that weapon is exhausted, unless that weapon
    equips a new default weapon for the player.
    
    --- POSSIBLE GAMEPLAY EXPLOIT
    - If a player has carried a limited use weapon into the start of a new
    round, and proceeds to not pick up a weapon that round, they will start
    the following round with that weapon again. This allows a player to hold
    onto a weapon for as long as they wish.

---
+++ Assets/Scripts/Items/Weapons/BombWeaponData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Bomb Weapon", menuName = "Item/Weapon/BombWeapon")]
+public class BombWeaponData : WeaponData<BombWeapon> {
+    [Header("Projectile Variables")]
+    public ProjectileData projectile;
+    public int projNum = 1;
+    public float projSpeed = 25f;
+
+    [Header("Bomb Variables")]
+    public GameObject bombPrefab;
+    public float explosionSize = 2f;                    // Used to set the scale of a Bomb GameObject.
+    [Range(0f, 1f)]                                     // This value is going to be put into a lerp equation, so it only needs to go from 0 to 1.
+    public float growthRate = .05f;
+    public int numberOfUses = 1;
+}
