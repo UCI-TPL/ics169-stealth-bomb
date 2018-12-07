c3a1172c4c7280746572d39a7ce51ecf10d506d9
Brian Lam <BrianLam4101@gmail.com>
2018-12-06 16:20:20 -0800

    Items Expire if Not Picked Up
    Items have a duration before they disappear
    - Items flash when they are almost expiring

--- Assets/Scripts/ItemContainer.cs
+++ Assets/Scripts/ItemContainer.cs
@@ -7,21 +7,40 @@ public class ItemContainer : MonoBehaviour {
     private ItemData data;
 
     private bool destroying = false;
+    [SerializeField]
+    private float duration = 10;
+    private float endTime;
+    [SerializeField]
+    private float flashDuration = 2;
+    [SerializeField]
+    private float flashesPerSec = 8;
+
+    private SpriteRenderer spriteRenderer;
 
     private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         if (data != null) // Set item sprite if item data already present, this would only happen is item was placed in level by editor
-            GetComponent<SpriteRenderer>().sprite = data.image;
+            spriteRenderer.sprite = data.image;
     }
 
     private void Start() {
         if (data == null) // Check to ensure item is present
             Debug.LogError(gameObject.name + " missing Item");
         StartCoroutine("DefaultCreateEff");
+        endTime = Time.time + duration;
+    }
+
+    private void Update() {
+        if (endTime - flashDuration <= Time.time) { // Item is about to be destroyed
+            spriteRenderer.color = new Color(1, 1, 1, Mathf.Cos((endTime - Time.time) * flashesPerSec * Mathf.PI * 2)+1); // Flashes in a cos wave pattern
+            if (endTime <= Time.time)
+                Destroy();
+        }
     }
 
     public void SetItemData(ItemData data) {
         this.data = data;
-        GetComponent<SpriteRenderer>().sprite = data.image;
+        spriteRenderer.sprite = data.image;
     }
 
     // If collider is a player, add item to player
@@ -43,7 +62,7 @@ public class ItemContainer : MonoBehaviour {
 
     // Effect that plays while the object is creating
     private IEnumerator DefaultCreateEff() {
-        GetComponent<Collider>().enabled = false; // Disable collision detections while creating
+        //GetComponent<Collider>().enabled = false; // Disable collision detections while creating
         float endTime = Time.time + 0.4045085f;
         while (true) {
             float scale = -Mathf.Pow((4f * (endTime - Time.time - 0.125f)), 2) + 1.25f;
@@ -53,7 +72,7 @@ public class ItemContainer : MonoBehaviour {
             yield return null;
         }
         transform.localScale = new Vector3(1, 1, 1);
-        GetComponent<Collider>().enabled = true; // Enable collisions when done creating
+        //GetComponent<Collider>().enabled = true; // Enable collisions when done creating
     }
 
     // Effect that plays while the object is destroying
