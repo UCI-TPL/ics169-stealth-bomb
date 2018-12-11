5e81ec291b4b052aa7bca344446600938a9d335b
Nikhil Sharma <nik41507@gmail.com>
2018-12-11 07:34:32 -0800

    Improved Cursor Tile Death
    -small changes, but now the tile underneath the tip of the cursor should die

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -16,6 +16,12 @@ public class GhostController : PlayerController {   //this inherits from PlayerC
     public ParabolaController ParabolaController;
     public GameObject ParabolaRoot;
 
+    public GameObject Point;
+
+    public float cooldown = 0.1f;
+
+  //  Camera camera = Ge
+
     private void Start()
     {   
         forward = Camera.main.transform.forward;
@@ -28,8 +34,6 @@ public class GhostController : PlayerController {   //this inherits from PlayerC
         CursorImage.color = playerColor;
         //rend.material.color = playerColor; //setting the player color based on playeNum 
         lastPosition = transform.position;
-
-
         //Vector3 ghostLocation = new Vector3(transform.position.x - 13f, transform.position.y, transform.position.z + 6f);
         Vector3 ghostLocation = new Vector3(1f, 8f, 20f);
 
@@ -37,30 +41,28 @@ public class GhostController : PlayerController {   //this inherits from PlayerC
         GhostBody.GetComponentsInChildren<Renderer>()[1].material.color = playerColor;
         GhostBody.GetComponent<ParabolaController>().Begin(GameObject.FindGameObjectWithTag("ghost-curve").gameObject); //this is like a  constructor used to attach the parabola root (which is pre-existing in the map)
         GhostBody.GetComponent<ParabolaController>().ParabolaRoot = GameObject.FindGameObjectWithTag("ghost-curve"); //The GhostBody is created and placed upon the paraboloa
-        //GhostBody.GetComponent<ParabolaController>().UpdatePosition(transform.position.z);
 
         input.controllers[player.playerNumber].attack.OnDown.AddListener(Activate);
 
     }
 
-
+    private float destoryTileTime = 0.0f;
     public void Activate()
     {
-        int layerMask = 1 << 11; //this makes sure that it can only detect the Ground layer
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
+
+        if (destoryTileTime <= Time.time)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Debug.Log("Did Hit something for sure yuuuup at "+hit.transform.position);
-            Tile temp = hit.transform.GetComponent<Tile>();
-            if(temp)
+            destoryTileTime = Time.time + cooldown;
+            int layerMask = 1 << 11; //this makes sure that it can only detect the Ground layer
+            RaycastHit hit;
+            if (Physics.Raycast(Point.transform.position, Vector3.down, out hit, Mathf.Infinity, layerMask))
             {
-                //Debug.Log("Hit a tile at : " + temp.position);
-                TileManager.tileManager.DestroyTiles(temp.position);
+                Tile temp = hit.transform.GetComponent<Tile>();
+                if (temp)
+                    TileManager.tileManager.DestroyTiles(temp.position);
             }
         }
     }
-
 
     private void FixedUpdate()
     {
