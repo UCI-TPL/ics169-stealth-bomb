49fda3bb99cc060461ce1d3ef37c212000633e91
Brian Lam <BrianLam4101@gmail.com>
2019-01-24 23:44:58 -0800

    Added not toooooo safe multi-threading back to inputmanager

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -98,10 +98,10 @@ public class InputManager : MonoBehaviour {
         ChangeControllerType(1, Controller.Type.TouchScreen);
 #endif
 
-//#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
-//        UpdateXInputStateThread = new Thread(UpdateXInputStateThreadLoop);
-//        UpdateXInputStateThread.Start();
-//#endif
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        UpdateXInputStateThread = new Thread(UpdateXInputStateThreadLoop);
+        UpdateXInputStateThread.Start();
+#endif
     }
 
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
@@ -821,14 +821,12 @@ public class InputManager : MonoBehaviour {
 
         // Update This controller's events
         public override void UpdateController() {
-            prevState = state;
-            state = GamePad.GetState(playerIndex);
-            // State is updated in a different thread
-            //if (stateUpdated)
-            //    UpdateState(state);
-            //if (isActive)
-            UpdateEvents();
-            //stateUpdated = true;
+            //State is updated in a different thread
+            if (stateUpdated)
+                UpdateState(state);
+            if (isActive)
+                UpdateEvents();
+            stateUpdated = true;
         }
 
         public void UpdateState(GamePadState state) {
