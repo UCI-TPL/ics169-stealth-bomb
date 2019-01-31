49cbe8ec6cbd87f08f093289ed8d7438d29ac3f8
d-guzman <dghost121@gmail.com>
2019-01-31 00:45:31 -0800

    Grenade Launcher modifications

--- Assets/Scripts/BombProjectileAddon.cs
+++ Assets/Scripts/BombProjectileAddon.cs
@@ -9,6 +9,7 @@ public class BombProjectileAddon : MonoBehaviour
     //public int maxBounces = 1;
     private int bounceNum = 0;
     public float arcHeight = 3.5f;
+    public float arcMoveSpeed = 2f;
     private float time = 0f;
     private bool bombStop = false;
 
@@ -26,18 +27,21 @@ public class BombProjectileAddon : MonoBehaviour
         {
             if (proj.getHasHit() == true)
             {
+                Debug.Log("Counting Bounce");
                 bounceNum++;
                 if (bounceNum <= 2)
                 {
                     proj.setHasHit(false);
-                    rb.detectCollisions = true;
                     rb.isKinematic = false;
+                    Invoke("resetCollision", .042f);        // Hold off setting the flag for ~2-3 frames to let the bomb come back up.
+
 
                     if (bounceNum == 2) { bombStop = true; }
                     else
                     {
                         proj.transform.rotation = Quaternion.Euler(proj.transform.rotation.eulerAngles.x, proj.transform.rotation.eulerAngles.y - 90f, proj.transform.rotation.eulerAngles.z);
-                        proj.GetComponent<Rigidbody>().velocity = proj.transform.forward * 8f;
+                        proj.GetComponent<Rigidbody>().velocity = proj.transform.forward * 12f;
+                        time = 0f;
                     }
                 }
             }
@@ -48,9 +52,16 @@ public class BombProjectileAddon : MonoBehaviour
     {
         if (!bombStop)
         {
-            time += Time.deltaTime;
+            time += Time.deltaTime * arcMoveSpeed;
             Vector3 movement = transform.position + new Vector3(0f, Mathf.Cos(time) * Time.deltaTime, 0f) * arcHeight;
             rb.MovePosition(movement);
         }
+    }
+
+    // This method is needed to prevent the bomb from going too deep into the level, and exploding twice immediately.
+    // There may be one instance where the bomb phases through a corner, so if that happens, check here.
+    private void resetCollision()
+    {
+        rb.detectCollisions = true;
     }
 }
