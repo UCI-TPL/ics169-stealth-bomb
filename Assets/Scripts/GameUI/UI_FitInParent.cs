15dc036b9475e9fbfcb525a388d9a86f4541d5f9
Brian Lam <BrianLam4101@gmail.com>
2019-02-14 23:37:57 -0800

    New Progress Screen Layout

---
+++ Assets/Scripts/GameUI/UI_FitInParent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+[RequireComponent(typeof(RectTransform))]
+public class UI_FitInParent : MonoBehaviour
+{
+    public float minAspectRatio;
+    public float maxAspectRatio;
+    private RectTransform rectTransform;
+    private RectTransform parentRectTransform;
+
+    private void Start() {
+        rectTransform = GetComponent<RectTransform>();
+        parentRectTransform = rectTransform.parent.GetComponent<RectTransform>();
+    }
+
+    void Update() {
+        switch(GetState()) {
+            case ScaleState.min:
+                rectTransform.sizeDelta = new Vector2(parentRectTransform.rect.width, parentRectTransform.rect.width / minAspectRatio);
+                break;
+            case ScaleState.max:
+                rectTransform.sizeDelta = new Vector2(parentRectTransform.rect.height * maxAspectRatio, parentRectTransform.rect.height);
+                break;
+            case ScaleState.middle:
+                rectTransform.sizeDelta = parentRectTransform.rect.size;
+                break;
+        }
+    }
+
+    private ScaleState GetState() {
+        float parentAspectRatio = parentRectTransform.rect.width / parentRectTransform.rect.height;
+        if (parentAspectRatio < minAspectRatio)
+            return ScaleState.min;
+        else if (parentAspectRatio > maxAspectRatio)
+            return ScaleState.max;
+        return ScaleState.middle;
+    }
+
+    private enum ScaleState { min, middle, max }
+}
---
+++ Assets/Scripts/GameUI/UI_LayoutGroupPercentPadding.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(LayoutGroup))]
+public class UI_LayoutGroupPercentPadding : MonoBehaviour {
+
+    private LayoutGroup layoutGroup;
+    [Header("Padding")]
+    public float left;
+    public float right;
+    public float top;
+    public float bottom;
+    public float spacing;
+    public Vector2 gridSpacing;
+    private RectTransform rectTransform;
+    private LayoutGroupType layoutGroupType;
+    private HorizontalLayoutGroup horizontalLayoutGroup;
+    private VerticalLayoutGroup verticalLayoutGroup;
+    private GridLayoutGroup gridLayoutGroup;
+
+    void Start() {
+        layoutGroup = GetComponent<LayoutGroup>();
+        rectTransform = GetComponent<RectTransform>();
+        // Save type of layout group so it does not have to be checked again later
+        if (layoutGroup.GetType() == typeof(HorizontalLayoutGroup)) {
+            layoutGroupType = LayoutGroupType.Horizontal;
+            horizontalLayoutGroup = (HorizontalLayoutGroup)layoutGroup;
+        }
+        else if (layoutGroup.GetType() == typeof(VerticalLayoutGroup)) {
+            layoutGroupType = LayoutGroupType.Vertical;
+            verticalLayoutGroup = (VerticalLayoutGroup)layoutGroup;
+        }
+        else if (layoutGroup.GetType() == typeof(GridLayoutGroup)) {
+            layoutGroupType = LayoutGroupType.Grid;
+            gridLayoutGroup = (GridLayoutGroup)layoutGroup;
+        }
+    }
+    
+    void Update() {
+        layoutGroup.padding = new RectOffset(Mathf.RoundToInt(left * rectTransform.rect.width), Mathf.RoundToInt(right * rectTransform.rect.width), Mathf.RoundToInt(top * rectTransform.rect.height), Mathf.RoundToInt(bottom * rectTransform.rect.height));
+        // Modify spacing depending on layout group type
+        switch (layoutGroupType) {
+            case LayoutGroupType.Horizontal:
+                horizontalLayoutGroup.spacing = spacing * rectTransform.rect.width;
+                break;
+            case LayoutGroupType.Vertical:
+                verticalLayoutGroup.spacing = spacing * rectTransform.rect.height;
+                break;
+            case LayoutGroupType.Grid:
+                gridLayoutGroup.spacing = gridSpacing * rectTransform.rect.size;
+                break;
+        }
+    }
+
+    private enum LayoutGroupType { Horizontal, Vertical, Grid }
+}
