4c53598ec5135dae906c3ad9b9b8b96f0ea6476a
Nikhil Sharma <nik41507@gmail.com>
2019-02-26 15:45:07 -0800

    Charge Particle Effects
    - There is a bunch of them
    
    -Right now they have different colors for each player but we could just give it one color if that looks better

--- Assets/Scripts/Items/Weapons/WeaponParticles.cs
+++ Assets/Scripts/Items/Weapons/WeaponParticles.cs
@@ -11,15 +11,25 @@ public class WeaponParticles
 
     public ParticleSystem[] Particles;
 
+    public ParticleSystem[] LoopParticles; //particles that loop after the charge is complete
+
     public ParticleSystem Circle; //making this first as a demo
 
     public void UpdateRoot(GameObject root, Color playerColor) //the root is the parent of all of the particle game objects;
     {
         Root = root;
-        Particles = new ParticleSystem[1];
-        Particles[0] = Root.transform.Find("Circle").GetComponent<ParticleSystem>();
-        Particles[0].GetComponent<Renderer>().material.color = playerColor * 1.5f;
-        //Particles[1] = Root.transform.Find("ChargedCenter").GetComponent<ParticleSystem>();
+        //Particles = new ParticleSystem[1];
+        Particles = Root.GetComponentsInChildren<ParticleSystem>();
+        foreach (ParticleSystem part in Particles)
+        {
+            part.GetComponent<Renderer>().material.color = playerColor * 2.5f;
+        }
+        //Particles[0] = Root.transform.Find("Center").GetComponent<ParticleSystem>();
+        //Particles[0].GetComponent<Renderer>().material.color = playerColor * 1.5f;
+        //Particles[1] = Root.transform.Find("Lines").GetComponent<ParticleSystem>();
+        //Particles[1].GetComponent<Renderer>().material.color = playerColor * 1.5f;
+        //Particles[1] = Root.transform.Find("Circle").GetComponent<ParticleSystem>();
+        //Particles[1].GetComponent<Renderer>().material.color = playerColor * 1.5f;
         //Circle = Root.transform.Find("Circle").GetComponent<ParticleSystem>(); //hardcoded for the hell of it hee hee
         //Circle.GetComponent<Renderer>().material.color = playerColor * 1.5f;
 
