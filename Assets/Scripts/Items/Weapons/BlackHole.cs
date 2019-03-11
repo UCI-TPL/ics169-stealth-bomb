d2eb41d6b65e0f567df3b141166b099929301274
Brian Lam <BrianLam4101@gmail.com>
2019-03-10 18:34:17 -0700

    Blackhole color now matches players'

--- Assets/Scripts/Items/Weapons/BlackHole.cs
+++ Assets/Scripts/Items/Weapons/BlackHole.cs
@@ -11,10 +11,18 @@ public class BlackHole : MonoBehaviour {
 	public int ignorePlayerIdx = -1;
 
 	private Color particleColor;
-	private ParticleSystem.MainModule main;
-	private ParticleSystem.TrailModule trail;
 
 	private SphereCollider collider;
+
+    [SerializeField]
+    private GameObject[] BlackholeEffects;
+    [SerializeField]
+    private Color vortexColor;
+    [SerializeField]
+    private Color backgroundColor;
+    [SerializeField]
+    [ColorUsage(true, true)]
+    private Color sparksColor;
 
 	private PlayerController[] caughtPlayers;
 
@@ -31,8 +39,10 @@ public class BlackHole : MonoBehaviour {
 
 	private float z = 14.72f;
 
-	private float defaultCenterScale = 0.35f;
+	private float defaultCenterScale = 0.25f;
 	private float defaultParticleSize = 0.1f;
+
+    private float radius;
 
 	void Awake() {
 		// material = gameObject.GetComponent<MeshRenderer>().materials[0];
@@ -42,8 +52,6 @@ public class BlackHole : MonoBehaviour {
 		// particleColor = gameObject.GetComponent<ParticleSystemRenderer>().trailMaterial;
 		// Debug.Log("trail material: " + particleColor.name);
 		// Debug.Log("materials: " + "length = " + materials.Length + ", " + materials[0].name /*+ ", " + materials[1].name*/);
-		main = gameObject.GetComponent<ParticleSystem>().main;
-		trail = gameObject.GetComponent<ParticleSystem>().trails;
 		collider = gameObject.GetComponent<SphereCollider>();
 	}
 
@@ -63,13 +71,12 @@ public class BlackHole : MonoBehaviour {
 	}
 
     IEnumerator DestroyDelayed(float duration) {
-        ParticleSystem p = gameObject.GetComponent<ParticleSystem>();
-        ParticleSystem.EmissionModule e = p.emission;
-        e.enabled = false;
-        Vector3 baseScale = center.transform.localScale/duration;
-        while (p.particleCount > 0) {
-            if (center.transform.localScale.x > 0)
-                center.transform.localScale = Vector3.MoveTowards(center.transform.localScale, Vector3.zero, baseScale.x * Time.deltaTime);
+        //ParticleSystem p = gameObject.GetComponent<ParticleSystem>();
+        //ParticleSystem.EmissionModule e = p.emission;
+        //e.enabled = false;
+        Vector3 baseScale = transform.localScale/duration;
+        while (transform.localScale.x > 0) {
+            transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.zero, baseScale.x * Time.deltaTime);
             yield return null;
         }
         Destroy(gameObject);
@@ -94,41 +101,28 @@ public class BlackHole : MonoBehaviour {
 
 	void OnTriggerExit(Collider other) {
 		if (other.gameObject.tag.Equals("Player")) {
-			if (Distance(transform.position, other.gameObject.transform.position) >= collider.radius + ((SphereCollider) other).radius) {
+			if (Vector3.Distance(transform.position, other.gameObject.transform.position) >= collider.radius + ((SphereCollider) other).radius) {
 				PlayerController pc = other.gameObject.GetComponent<PlayerController>();
 				caughtPlayers[pc.player.playerNumber] = null;
 			}
 		}
 	}
 
-	private float Distance(Vector3 p1, Vector3 p2) {
-		return Mathf.Sqrt(Squared(p2.x - p1.x) + Squared(p2.y - p1.y) + Squared(p2.z - p1.z));
-	}
-
-	private float Squared(float value) {
-		return value * value;
-	}
-
 	// helper method that defines how players are pulled towards the center of the black hole
 	private void PullPlayer(PlayerController player) {
-		float dirX = transform.position.x - player.gameObject.transform.position.x;
-		float dirY = transform.position.y - player.gameObject.transform.position.y;
-		float dirZ = transform.position.z - player.gameObject.transform.position.z;
-		// float dirX = player.gameObject.transform.position.x - transform.position.x;
-		// float dirY = player.gameObject.transform.position.y - transform.position.y;
-		// float dirZ = player.gameObject.transform.position.z - transform.position.z;
-		player.Knockback((new Vector3(dirX, 0.0f, dirZ).normalized /*+ Vector3.up*0.25f*/).normalized * blackHoleGravity, showEffect: false);
-		// player.velocity = new Vector3(transform.position.x - player.position.x, transform.position.y - player.position.y, transform.position.z - player.position.z).normalized * blackHoleGravity;
+        Vector3 pullVector = Vector3.Scale(transform.position - player.transform.position, new Vector3(1, 0, 1));
+		player.Knockback(pullVector.normalized * (1 - pullVector.magnitude / radius) * blackHoleGravity, showEffect: false);
 	}
 
 	// call to set parameters of black hole object
 	public void SetupBlackHole(float radius, int playerNum, float gravity, float duration, Color color) {
-		// gameObject.transform.localScale = new Vector3(radius*2, radius*2, radius*2);
-		// Apply radius to all concerned components and children of black hole: particle system, black hole (main) collider, and black hole center
-		ParticleSystem.ShapeModule shape = gameObject.GetComponent<ParticleSystem>().shape;
-		shape.radius = radius;
-		main.startSizeMultiplier = radius * defaultParticleSize * 0.5f;
-		trail.widthOverTrailMultiplier = radius;
+        // gameObject.transform.localScale = new Vector3(radius*2, radius*2, radius*2);
+        // Apply radius to all concerned components and children of black hole: particle system, black hole (main) collider, and black hole center
+        //ParticleSystem.ShapeModule shape = gameObject.GetComponent<ParticleSystem>().shape;
+        //shape.radius = radius;
+        this.radius = radius;
+        foreach (GameObject g in BlackholeEffects)
+            g.transform.localScale = Vector3.one * radius * 2;
 		collider.radius = radius;
 		center.transform.localScale = new Vector3(radius*defaultCenterScale, radius*defaultCenterScale, radius*defaultCenterScale);
 
@@ -141,10 +135,20 @@ public class BlackHole : MonoBehaviour {
 	}
 
 	public void SetupColor(Color color) {
-		Color HDRColor = color * 2.0f;
+        Color.RGBToHSV(color, out float targetH, out float tempS, out float tempV);
+
+        Color.RGBToHSV(vortexColor, out float H, out float S, out float V);
+        BlackholeEffects[0].GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(targetH, S, V);
+
+        Color.RGBToHSV(backgroundColor, out H, out S, out V);
+        BlackholeEffects[1].GetComponent<MeshRenderer>().material.color = Color.HSVToRGB(targetH, S, V);
+
+        Color.RGBToHSV(sparksColor, out H, out S, out V);
+        BlackholeEffects[2].GetComponent<MeshRenderer>().material.SetColor("_SparkColor", Color.HSVToRGB(targetH, S, V, true));
+
 		// Debug.Log("has material = " + (particleColor != null));
         // particleColor.SetColor("Color_E025656E", HDRColor);
-		main.startColor = color;
+		// main.startColor = color;
 		// trail.colorOverTrail = Color.red;
 		// main.startColor = HDRColor;
 		// trail.colorOverLifetime = HDRColor;
