75624123013e9a762132fc98a93f737fd5509868
Brian Lam <BrianLam4101@gmail.com>
2019-03-12 02:20:02 -0700

    Tiles are always visually full health in editor

---
+++ Assets/Scripts/Tiles/Editor/FixTileDamageEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+class FixTileDamageEditor {
+    static FixTileDamageEditor() {
+        EditorApplication.update += Update;
+    }
+
+    static void Update() {
+        Color[] colorArray = new Color[] { new Color(0, 0, 0, 1) };
+        Texture2D t = new Texture2D(1, 1, TextureFormat.RGBA32, false, true);
+        t.SetPixels(colorArray);
+        t.Apply();
+        Shader.SetGlobalTexture(Shader.PropertyToID("_TileDamageMap"), t);
+        Shader.SetGlobalVector(Shader.PropertyToID("_TileMapSize"), Vector3.one);
+    }
+}
+
+
