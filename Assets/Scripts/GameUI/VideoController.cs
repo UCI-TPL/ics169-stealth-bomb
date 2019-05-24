8daf75093a28444c2f94284f96d0f672081e21fc
David Jamgochian <jammer888@verizon.net>
2019-05-23 18:46:23 -0700

    Added rough version of video playing in background for menu.
    NOTE:
    1) video clip is not final and will be replaced!!!!! Also, the video clip is not included in the push because the file is too big right now. To add your own video, import the video you want, and assign it to the video clip variable of the video player component of the "Video Player" object.
    
    2) to TURN OFF of the video player and restore the previous background:
    1. Uncheck the ACTIVE box for the "Video Player" object.
    2. Uncheck the ACTIVE box for the "VideoDisplayTexture" object.

---
+++ Assets/Scripts/GameUI/VideoController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+// Borrowed and modified from code shown in https://www.youtube.com/watch?v=zCq0Jt6m8BQ
+// Source: YouTube
+// Creator/Author: Mir Imad
+// Video: "Unity Play Video On Canvas | Unity UI Video Player Component | Unity 5 Tutorial | Unity Video"
+public class VideoController : MonoBehaviour
+{
+    public RawImage image;
+    public VideoPlayer vidPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    // Borrowed and modified from code shown in https://www.youtube.com/watch?v=zCq0Jt6m8BQ
+    IEnumerator PlayVideo() {
+        vidPlayer.Prepare();
+        WaitForSeconds waitForSecs = new WaitForSeconds(1);
+
+        while (!vidPlayer.isPrepared) {
+            yield return waitForSecs;
+            break;
+        }
+
+        image.texture = vidPlayer.texture;
+        vidPlayer.Play();
+    }
+}
