e1a14422694e8622a4cbf282f1dd3e4a655093ea
Brian Lam <BrianLam4101@gmail.com>
2019-05-25 16:35:42 -0700

    Arrow Tilts with trajectory

---
+++ Assets/Scripts/Items/Weapons/ArrowTiltRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ArrowTiltRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform renderer;
+
+    private Rigidbody _rigidbody;
+    private Rigidbody rigidbody { get { return _rigidbody ?? GetComponent<Rigidbody>(); } }
+
+    [SerializeField]
+    [Tooltip("Rotations per sec")]
+    private float rotationSpeed;
+
+    private void FixedUpdate() {
+        Vector3 right = Vector3.Cross(Vector3.up, rigidbody.velocity);
+        renderer.rotation = Quaternion.LookRotation(right, Vector3.Cross(rigidbody.velocity, right));
+        renderer.Rotate(rigidbody.velocity, Time.time * rotationSpeed * 360, Space.World);
+    }
+}
